55981c0c7986867d994eda4ad34c36bd520612ab
Emmanuel <alan_varela1125@yahoo.com.ar>
2025-08-10 16:21:01 -0300

    creacion hello world

---
+++ hello.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+
+
+  res.end('Hello World!');
+});
+
+server.listen(3000, () => {
+  console.log('Servidor escuchando en http://localhost:3000');
+});
